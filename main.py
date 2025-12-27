90ed77e1d774bcb40090aa7895f410a699f0baca
matmatr4 <matylda.matracka@icloud.com>
2025-12-27 18:02:37 +0100

    imports

---
+++ main.py
@@ -0,0 +1,6 @@
+import sys
+import requests 
+from PyQt5.QtWidgets import QApplication, QLabel, QLineEdit, QPushButton, QVBoxLayout, QMainWindow
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
+
