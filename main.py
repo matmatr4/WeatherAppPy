79397fbe45f223e66cf69ad0453113d696e951b7
matmatr4 <matylda.matracka@icloud.com>
2025-12-29 14:38:00 +0100

    API retrieval and error detection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import requests 
-from PyQt5.QtWidgets import QApplication, QLabel, QLineEdit, QPushButton, QVBoxLayout, QMainWindow
+from PyQt5.QtWidgets import QApplication, QLabel, QLineEdit, QPushButton, QVBoxLayout, QMainWindow, QMessageBox
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 
@@ -8,7 +8,7 @@ class WeatherApp (QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.cityVar = "City"
+        self.cityDisp = "City"
 
         self.setWindowTitle("Weather App")
         self.setGeometry(100,100,600,800)
@@ -57,7 +57,7 @@ class WeatherApp (QMainWindow):
         self.WeatherPanel.setGeometry(20,120,560,660)
         self.WeatherPanel.setStyleSheet("background-color: rgba(0, 0, 0, 90)")
 
-        self.City = QLabel(self.cityVar, self)
+        self.City = QLabel(self.cityDisp, self)
         self.City.setGeometry(20,150,560,80)
         self.City.setStyleSheet("font-size: 70px;"
                                 "color: white;"
@@ -120,6 +120,9 @@ class WeatherApp (QMainWindow):
         self.Windspeed.setStyleSheet("font-size: 15px;"
                                      "font-weight: bold;")
 
+        self.ErrorMsgBox = QMessageBox(self)
+        self.ErrorMsgBox.setIcon(QMessageBox.Warning)
+
 
     def initUI(self):
         pass
@@ -130,24 +133,65 @@ class WeatherApp (QMainWindow):
         cityIn = self.CitySearch.text()
         url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{cityIn}?key={personalApiKey}"
         
+        try:
+            response = requests.get(url)
+            response.raise_for_status() #so that an error is raised if response is bad
+            data = response.json()
+            self.displayWeather(data)
         
-        response = requests.get(url)
-        data = response.json()
+        #catches errors with API response received
+        except requests.exceptions.HTTPError:
+            match response.status_code:
+                case 400:
+                    print("400 BAD REQUEST: Invalid request format or parameters")
+                    self.displayError("400 BAD REQUEST: Invalid request format or parameters", "400 Error")
+                case 401:
+                    print("401 UNAUTHORIZED: API key or account problem.")
+                    self.displayError("401 UNAUTHORIZED: API key or account problem", "401 Error")
+                case 404: 
+                    print("404 NOT FOUND: Endpoint does not exist.")
+                    self.displayError("404 NOT FOUND: Endpoint does not exist", "404 Error")
+                case 429:
+                    print("429 TOO MANY REQUESTS: Rate limit exceeded.")
+                    self.displayError("429 TOO MANY REQUESTS: Rate limit exceeded", "429 Error")
+                case 500:
+                    print("500 INTERNAL SERVER ERROR: Server problem.")
+                    self.displayError("500 INTERNAL SERVER ERROR: Server problem", "500 Error")
+                case _:
+                    print(f"HTTP error occurred: {response.status_code}")
+                    self.displayError("HTTP error occurred: {response.status_code}", "HTTP Error")
 
+        #catches network errors
+        except requests.exceptions.ConnectionError:
+            print("Network or connection error")
+            self.displayError("Network or connection error", "Network/Connection Error")
+
+        #catches timeout errors
+        except requests.exceptions.Timeout:
+            print("Request time out")
+            self.displayError("Request time out", "Request Time Out Error")
+
+        #catches URL errors
+        except requests.exceptions.TooManyRedirects:
+            print("URL error, too many or wrong redirects")
+            self.displayError("URL error, too many or wrong redirects", "URL Error")
+
+        #catches any other errors
+        except requests.exceptions.RequestException as e:
+            print(f"Other error occurred:{e}")
+            self.displayError(f"Other error occurred:{e}", "Error")
+           
         #print(data['resolvedAddress'])
         #print(data['days'][0]['temp'])
 
-        if response.status_code == 200:
-            print(response.status_code)
-            self.displayWeather(data)
-        else:
-            print("error") 
+    #displays an error message for the user after an error occurs
+    def displayError(self, ErrorName, ErrorTitle):
+        self.ErrorMsgBox.setText(ErrorName)
+        self.ErrorMsgBox.setWindowTitle(ErrorTitle)
+        self.ErrorMsgBox.exec_()
 
-    def displayError(self):
-        pass
-
-    def displayWeather(self):
-        pass
+    def displayWeather(self, data):
+        print(data)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
